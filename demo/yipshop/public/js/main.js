57e6f8dfe3064099f0b6433e285b1b432f3ac29c
matcaolanh <vantienlam.dongthap@gmail.com>
2017-04-07 16:26:16 +0700

    fix toggle error yipshop

--- demo/yipshop/public/js/main.js
+++ demo/yipshop/public/js/main.js
@@ -18,14 +18,10 @@ $('.list-nav-title').click(function () {
     
     if($(this).next().hasClass('respove_show'))
     {
-        $('#menu_responsive > span').removeClass('fa-times');
-        $('#menu_responsive > span').addClass('fa-navicon');
         $(this).next().removeClass('respove_show');
     }
     else
     {
-        $('#menu_responsive > span').removeClass('fa-navicon');
-        $('#menu_responsive > span').addClass('fa-times');
         $(this).next().addClass('respove_show');
     }
 });
