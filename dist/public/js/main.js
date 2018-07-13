5f886501c81a90698287ad63866821554bbb8932
tasynguyen3894 <thaisang.nguyen3894@gmail.com>
2018-07-13 16:13:32 +0700

    Remove node_modules

--- dist/public/js/main.js
+++ dist/public/js/main.js
@@ -1,15 +1,26 @@
-
-$('#menu_responsive').click(function () {
-    if($('#header_menu').hasClass('respove_show'))
-    {
-        $('#menu_responsive > span').removeClass('fa-times');
-        $('#menu_responsive > span').addClass('fa-navicon');
-        $('#header_menu').removeClass('respove_show');
-    }
-    else
-    {
-        $('#menu_responsive > span').removeClass('fa-navicon');
-        $('#menu_responsive > span').addClass('fa-times');
-        $('#header_menu').addClass('respove_show');
-    }
+$('#nav-info-toggle').click(function () {
+	if($('#nav-info').hasClass('show-info')) {
+		$('#nav-info').removeClass('show-info');
+	} else {
+		$('#nav-info').addClass('show-info');
+	}
 });
+
+$('#nav-info-mobile-open').click(function () {
+	if($('.navbar-fixed').hasClass('show-info')) {
+		$('.navbar-fixed').removeClass('show-info');
+	} else {
+		$('.navbar-fixed').addClass('show-info');
+	}
+});
+
+
+$('#nav-info-mobile-close').click(function () {
+	if($('.navbar-fixed').hasClass('show-info')) {
+		$('.navbar-fixed').removeClass('show-info');
+	} else {
+		$('.navbar-fixed').addClass('show-info');
+	}
+});
+
+
