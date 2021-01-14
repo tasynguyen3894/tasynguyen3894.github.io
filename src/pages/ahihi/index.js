36646351db4c45b7d96760433ee456b8acd892fc
tasynguyen3894 <thaisang.nguyen3894@gmail.com>
2021-01-14 22:42:03 +0700

    hehe

---
+++ src/pages/ahihi/index.js
@@ -0,0 +1,65 @@
+module.exports = {
+    data: {
+        posts: [
+            {
+                url: "chi-la-nghe-nham-thoi.html",
+                title: "Chỉ là nghe nhầm thôi",
+                tags: [
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Nếu nói đến game thẻ bài trong một bài viết hơi hướng hoài cổ thì chắc nhiều người sẽ nghĩ tới Yugi Oh hay là Pokemon gì đó. Nhưng thú thiệt là mấy cái đó quá xa vời với bọn con nít chợ xã bọn tôi. Cái trò mà hôm nay tôi kể là một trò sưu tầm thẻ làm từ... bao thuốc lá"
+            },
+            {
+                url: "di-giua-thanh-pho-khong-co-em.html",
+                title: "Đi giữa thành phố không có em",
+                tags: [
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Đôi lúc có những thứ hai làm cho người ta lười biếng tới khó tả, thứ hai hôm nay là một thứ hai như thế"
+            },
+            {
+                url: "qua-dem-o-cong-ty-va-mot-so-thu-linh-tinh-tu-facebook.html",
+                title: "Qua đêm ở công ty và một số thứ linh tinh từ Facebook",
+                tags: [
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Hè về, mưa lác đác và đêm một mình giữa căn phòng nhỏ. Mọi thứ ảm đạm chẳng còn gì để nói. Cũng lâu lâu rồi chẳng biết viết gì, nay rãnh rỗi tôi gõ lại vài dòng cho vui."
+            },
+            {
+                url: "la-dieu-bong.html",
+                title: "Lá diêu bông",
+                tags: [
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Được nghỉ hai ngày cuối tuần, không có cái hẹn cà phê hay cái gì đại loại thế. Hai ngày cuối tuần của mình là hai ngày chỉ làm bạn với cái PC."
+            },
+            {
+                url: "nhung-bong-hinh-di-vang.html",
+                title: "Những bóng hình dĩ vãng",
+                tags: [
+                    {text: "lan man", code: "lan-man"},
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Đã lâu lắm rồi tôi không viết cái gì nửa, hôm nay tự nhiên rãnh rỗi nên ngồi trước máy tính gõ vài dòng kể về đời sống ăn nhậu tai hại của mình."
+            },
+            {
+                url: "cai-thau-bang-cu-cua-cha.html",
+                title: "Cái thâu băng cũ của cha",
+                tags: [
+                    {text: "lan man", code: "lan-man"},
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Nháy mắt vài cái mà nay đã là ngày đầu tiên của năm 2020 rồi. Mình lại ngồi lại gõ từng dòng nói về 2019 (như năm rồi nói về 2018)."
+            },
+            // {
+            //     url: "toi-nghe-the-beatles-trong-nhung-ngay-chan-chuong-cua-su-nghiep.html",
+            //     title: "Tôi nghe The Beatles trong những ngày chán chường của sự nghiệp",
+            //     tags: [
+            //         {text: "lan man", code: "lan-man"},
+            //         {text: "my stories", code: "my-stories"}
+            //     ],
+            //     content: "Có vẻ những ngày gần đây tôi lười biếng hơn, ù lì hơn và chán chường hơn thường lệ, mà cũng có thể là thường lệ tôi cũng đã thế nhưng tôi cố lừa dối bản thân mình rằng tôi là một thằng siêng năng."
+            // }
+        ]
+    }
+}
