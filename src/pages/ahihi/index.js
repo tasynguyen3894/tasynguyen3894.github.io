d897c50eedff79411ff86bf292ed485dbd2b3885
tasynguyen3894 <thaisang.nguyen3894@gmail.com>
2021-01-16 20:55:01 +0700

    hehe

--- src/pages/ahihi/index.js
+++ src/pages/ahihi/index.js
@@ -51,15 +51,15 @@ module.exports = {
                 ],
                 content: "Nháy mắt vài cái mà nay đã là ngày đầu tiên của năm 2020 rồi. Mình lại ngồi lại gõ từng dòng nói về 2019 (như năm rồi nói về 2018)."
             },
-            // {
-            //     url: "toi-nghe-the-beatles-trong-nhung-ngay-chan-chuong-cua-su-nghiep.html",
-            //     title: "Tôi nghe The Beatles trong những ngày chán chường của sự nghiệp",
-            //     tags: [
-            //         {text: "lan man", code: "lan-man"},
-            //         {text: "my stories", code: "my-stories"}
-            //     ],
-            //     content: "Có vẻ những ngày gần đây tôi lười biếng hơn, ù lì hơn và chán chường hơn thường lệ, mà cũng có thể là thường lệ tôi cũng đã thế nhưng tôi cố lừa dối bản thân mình rằng tôi là một thằng siêng năng."
-            // }
+            {
+                url: "tren-dai-lo-ngoai-cua-kinh-den-can-phong-nho-mua-va-su-vo-dinh.html",
+                title: "Trên đại lộ, ngoài cửa kính đến căn phòng nhỏ. Mưa và sự vô định.",
+                tags: [
+                    {text: "lan man", code: "lan-man"},
+                    {text: "my stories", code: "my-stories"}
+                ],
+                content: "Có vẻ những ngày gần đây tôi lười biếng hơn, ù lì hơn và chán chường hơn thường lệ, mà cũng có thể là thường lệ tôi cũng đã thế nhưng tôi cố lừa dối bản thân mình rằng tôi là một thằng siêng năng."
+            }
         ]
     }
 }
